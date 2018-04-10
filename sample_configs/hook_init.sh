223704543cb33fdd41fce884eb599fc84711ac51
Alexandre Bourget <alex@diagram.ca>
2018-04-10 00:09:04 -0400

    Took out sample configs, reach for `eos-bios-launch-data`  instead.

--- sample_configs/hook_init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Compile the wast file"
-/home/abourget/build/eos/build/wat2wasm /home/abourget/build/eos/build/contracts/eosio.system/eosio.system.wast -o /home/abourget/build/eos/build/contracts/eosio.system/eosio.system.wasm
