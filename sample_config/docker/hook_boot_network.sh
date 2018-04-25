602182dbfe9935fe88ae59ebe119a1a4174f112d
Alexandre Bourget <alex@diagram.ca>
2018-04-25 12:28:19 -0400

    Added instructions in the hooks.

--- sample_config/docker/hook_boot_network.sh
+++ sample_config/docker/hook_boot_network.sh
@@ -17,8 +17,10 @@ echo "producer-name = eosio" >> config.ini
 echo "enable-stale-production = true" >> config.ini
 echo "private-key = [\"$2\",\"$3\"]" >> config.ini
 
+echo "Removing old nodeos data (you might be asked for your sudo password)..."
 sudo rm -rf /tmp/nodeos-data
 
+echo "Running 'nodeos' through Docker."
 docker run -ti --rm --detach --name nodeos-bios \
        -v `pwd`:/etc/nodeos -v /tmp/nodeos-data:/data \
        -p 8888:8888 -p 9876:9876 \
@@ -26,6 +28,10 @@ docker run -ti --rm --detach --name nodeos-bios \
        /opt/eosio/bin/nodeos --data-dir=/data \
                              --genesis-json=/etc/nodeos/genesis.json \
                              --config-dir=/etc/nodeos
+
+echo ""
+echo "   View logs with: docker logs -f nodeos-bios"
+echo ""
 
 echo "Waiting for nodeos to launch through Docker"
 sleep 3
