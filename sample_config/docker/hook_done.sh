53050290edae747d167cd60706abbccda81f4415
Alexandre Bourget <alex@diagram.ca>
2018-04-25 13:07:23 -0400

    Added instructions to view logs.

--- sample_config/docker/hook_done.sh
+++ sample_config/docker/hook_done.sh
@@ -4,3 +4,10 @@
 # no parameters
 
 echo "All done, anything else you want to do ? Call regproducer somehow?"
+echo ""
+echo "Don't forget to view your logs with:"
+echo "   docker logs -f nodeos-bios"
+echo ""
+echo "Kill your node with:"
+echo "   docker kill nodeos-bios"
+echo ""
