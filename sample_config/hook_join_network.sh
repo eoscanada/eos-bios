5d460cf11d24a854f37637c6a80bcb519ad5255a
Alexandre Bourget <alex@bourget.cc>
2018-05-16 16:29:24 -0400

    don't fail if killall fails.

--- sample_config/hook_join_network.sh
+++ sample_config/hook_join_network.sh
@@ -21,7 +21,7 @@ PRIVKEY=5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
 
 
 echo "Killing running nodes"
-killall nodeos
+killall nodeos || true
 
 echo "Removing old nodeos data (you might be asked for your sudo password)..."
 sudo rm -rf /tmp/nodeos-data
