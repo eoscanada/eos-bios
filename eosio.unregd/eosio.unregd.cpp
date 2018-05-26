94f8c7eeab3321dd419db10cb878d1ff36cefc58
Alexandre Bourget <alex@bourget.cc>
2018-05-25 23:48:06 -0400

    Updated unregd so it doesn't drain performances with the poor man's search!

--- eosio.unregd/eosio.unregd.cpp
+++ eosio.unregd/eosio.unregd.cpp
@@ -1,5 +1,3 @@
-#include <algorithm>
-
 #include "eosio.unregd.hpp"
 
 using eosio::unregd;
@@ -15,32 +13,24 @@ void unregd::add(const ethereum_account& ethereum_account, const asset& balance)
   auto symbol = balance.symbol;
   eosio_assert(symbol.is_valid() && symbol == EOS_SYMBOL, "balance must be EOS token");
 
-  // TODO: What to do with existing mapping? Replace? Error?
-  auto itr = find_ethereum_account(ethereum_account);
-  if (itr == accounts.end()) {
-    accounts.emplace(_self, [&](auto& account) {
-      account.id = accounts.available_primary_key();
-      account.ethereum_account = ethereum_account;
-      account.balance = balance;
-    });
-  } else {
-    accounts.modify(itr, _self, [&](auto& account) {
-      account.ethereum_account = ethereum_account;
-      account.balance = balance;
-    });
-  }
+  eosio_assert(ethereum_account.length() == 42, "Ethereum account should have exactly 42 characters");
+
+  update_account(ethereum_account, [&](auto& account) {
+    account.ethereum_account = ethereum_account;
+    account.balance = balance;
+  });
 }
 
-/**
- * For now, in `multi_index`, there is no possibility to create a secondary index
- * based on a `std::string` or a fixed sized array (unless a key256). As such,
- * for now, let's do a poor man search to find a matching account.
- *
- * TODO: Use a key256 type instead. The `ethereum_account` (40 hex characters,
- * 80 bytes) would then be the 80 first bytes of the key and the rest would be
- * padded with zeros creating a key256 from an ethereum account.
- */
-unregd::accounts_index::const_iterator unregd::find_ethereum_account(const ethereum_account& ethereum_account) const {
-  return std::find_if(accounts.cbegin(), accounts.cend(),
-                      [&](auto& account) { return account.ethereum_account == ethereum_account; });
+void unregd::update_account(const ethereum_account& ethereum_account, const function<void(account&)> updater) {
+  auto index = accounts.template get_index<N(ethereum_account)>();
+
+  auto itr = index.find(compute_ethereum_account_key256(ethereum_account));
+  if (itr == index.end()) {
+    accounts.emplace(_self, [&](auto& account) {
+      account.id = accounts.available_primary_key();
+      updater(account);
+    });
+  } else {
+    index.modify(itr, _self, [&](auto& account) { updater(account); });
+  }
 }
--- eosio.unregd/eosio.unregd.hpp
+++ eosio.unregd/eosio.unregd.hpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <string>
 
 #include <eosiolib/asset.hpp>
@@ -14,6 +15,7 @@ typedef std::string ethereum_account;
 using eosio::const_mem_fun;
 using eosio::indexed_by;
 using eosio::key256;
+using std::function;
 using std::string;
 
 namespace eosio {
@@ -30,6 +32,27 @@ class unregd : public contract {
   static const uint64_t EOS_PRECISION = 4;
   static const asset_symbol EOS_SYMBOL = S(EOS_PRECISION, EOS);
 
+  static uint8_t hex_char_to_uint(char character) {
+    const int x = character;
+
+    return (x <= 57) ? x - 48 : (x <= 70) ? (x - 65) + 0x0a : (x - 97) + 0x0a;
+  }
+
+  static key256 compute_ethereum_account_key256(const ethereum_account& ethereum_account) {
+    uint8_t ethereum_key[20];
+    const char* characters = ethereum_account.c_str();
+
+    // The ethereum account starts with 0x, let's skip those by starting at i = 2
+    for (uint64_t i = 2; i < ethereum_account.length(); i += 2) {
+      const uint64_t index = (i / 2) - 1;
+
+      ethereum_key[index] = 16 * hex_char_to_uint(characters[i]) + hex_char_to_uint(characters[i + 1]);
+    }
+
+    const uint32_t* p32 = reinterpret_cast<const uint32_t*>(&ethereum_key);
+    return key256::make_from_word_sequence<uint32_t>(p32[0], p32[1], p32[2], p32[3], p32[4]);
+  }
+
   //@abi table accounts i64
   struct account {
     uint64_t id;
@@ -37,13 +60,17 @@ class unregd : public contract {
     asset balance;
 
     uint64_t primary_key() const { return id; }
+    key256 by_ethereum_account() const { return unregd::compute_ethereum_account_key256(ethereum_account); }
 
     EOSLIB_SERIALIZE(account, (id)(ethereum_account)(balance))
   };
 
-  typedef eosio::multi_index<TABLE(accounts), account> accounts_index;
+  typedef eosio::multi_index<
+      TABLE(accounts), account,
+      indexed_by<N(ethereum_account), const_mem_fun<account, key256, &account::by_ethereum_account>>>
+      accounts_index;
 
-  accounts_index::const_iterator find_ethereum_account(const ethereum_account& ethereum_account) const;
+  void update_account(const ethereum_account& ethereum_account, const function<void(account&)> updater);
 
   accounts_index accounts;
 };
